b901e8afbcc506615d5d1aa6f9249077f83760d5
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-10-27 20:50:29 +0100

    Update

---
+++ src/components/blog/blog-item/index.jsx
@@ -0,0 +1,50 @@
+import React, {useEffect, useState } from "react";
+import { Card } from "react-bootstrap";
+import BlogAuthor from "../blog-author";
+import { Link } from "react-router-dom";
+import "./styles.css";
+
+function BlogItem(props) {
+  const [blogDetails, setBlogDetails] = useState([]);
+
+  // fetch blogs
+  useEffect(() => {
+    fetchBlogsData()
+  }, []);
+
+  const fetchBlogsData = async () => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_BE_PROD_URL}/blogs`)
+      console.log(response);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        setBlogDetails(data);
+        console.log(blogDetails);
+        console.log("Blog data: ", data);
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  
+
+    
+  return (
+    <Link to={`/blog/${props._id}`} className="blog-link">
+      <Card className="blog-card">
+        <Card.Img variant="top" src={props.cover} className="blog-cover" />
+        <Card.Body>
+          <Card.Title>{props.title}</Card.Title>
+        </Card.Body>
+        <Card.Footer>
+          <BlogAuthor {...props.author} />
+        </Card.Footer>
+      </Card>
+    </Link>
+  );
+  
+
+}
+
+export default BlogItem
