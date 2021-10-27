5cd6c254fa6ca2e45e3f55405dc94c2c6f81ad31
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-10-27 22:29:21 +0100

    update

--- src/components/blog/blog-item/index.jsx
+++ src/components/blog/blog-item/index.jsx
@@ -1,50 +1,26 @@
-import React, {useEffect, useState } from "react";
+
+import React, { Component } from "react";
 import { Card } from "react-bootstrap";
 import BlogAuthor from "../blog-author";
 import { Link } from "react-router-dom";
 import "./styles.css";
 
-function BlogItem(props) {
-  const [blogDetails, setBlogDetails] = useState([]);
 
-  // fetch blogs
-  useEffect(() => {
-    fetchBlogsData()
-  }, []);
-
-  const fetchBlogsData = async () => {
-    try {
-      const response = await fetch(`${process.env.REACT_APP_BE_PROD_URL}/blogs`)
-      console.log(response);
-      if (response.ok) {
-        const data = await response.json();
-        console.log(data);
-        setBlogDetails(data);
-        console.log(blogDetails);
-        console.log("Blog data: ", blogDetails);
-      }
-    } catch (error) {
-      console.log(error)
-    }
+export default class BlogItem extends Component {
+  render() {
+    const { title, cover, author, _id } = this.props;
+    return (
+      <Link to={`/blog/${_id}`} className="blog-link">
+        <Card className="blog-card">
+          <Card.Img variant="top" src={cover} className="blog-cover" />
+          <Card.Body>
+            <Card.Title>{title}</Card.Title>
+          </Card.Body>
+          <Card.Footer>
+            <BlogAuthor {...author} />
+          </Card.Footer>
+        </Card>
+      </Link>
+    );
   }
-  
-
-    
-  return (
-    <Link to={`/blog/${props._id}`} className="blog-link">
-      <Card className="blog-card">
-        <Card.Img variant="top" src={cover} className="blog-cover" />
-        <Card.Body>
-          <Card.Title>{props.title}</Card.Title>
-        </Card.Body>
-        <Card.Footer>
-          <BlogAuthor {...props.author} />
-        </Card.Footer>
-      </Card>
-    </Link>
-  );
-  
-
 }
-
-export default BlogItem
